bca0b69cd2975b3b5905fe60964c6b7a3f00248d
Sergey Timofeev <ug.tiser@gmail.com>
2016-08-27 03:21:44 +0300

    Everything clear

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,11 +2,7 @@ import {Component} from '@angular/core'
 
 @Component({
   selector: "catitems",
-  template: `<h1>{{ title }}</h1>
-            <ul>
-              <li *ngFor="#list of categories">{{list}}</li>
-            </ul>
-            `
+  templateUrl: "./app/app.component.html"
 })
 
 export class AppComponent {
